87df7a9a83677581125c124476d01e83e4507ed8
Fırat Yılmaz <yilmaz8596@gmail.com>
2026-04-17 11:58:45 +0300

    update ci.yml and index.ts

--- api/src/util/index.ts
+++ api/src/util/index.ts
@@ -1,5 +1,5 @@
 // Note: avoid re-exporting generateOtp to prevent module-resolution issues in CI
-export * from './generateTokens';
-export * from './sendEmail';
-export * from './verifyOtp';
-export * from './tryCatch';
+export * from "./generateTokens";
+export * from "./sendEmail";
+export * from "./verifyOtp";
+export * from "./tryCatch";
