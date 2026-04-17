031327f4766f814342ae6956df622dbf4cdfe2b0
Fırat Yılmaz <yilmaz8596@gmail.com>
2026-04-17 14:20:37 +0300

    fix(ts): remove unused React default import in PrivateRoute

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import useStore from "../store/useStore";
 
